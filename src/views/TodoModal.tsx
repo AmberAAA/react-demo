d837310930747bdfce5f3905105777a698c91a98
AmberAAA <qe-d@163.com>
2018-12-06 20:40:58 +0800

    new hope we can add, delete, modify todo list; release v0.0.3

--- src/views/TodoModal.tsx
+++ src/views/TodoModal.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import {TODO} from "../module";
 import {Checkbox} from 'antd';
 import "antd/lib/checkbox/style/css";
+import "./TodoModal.css"
+import Button from "antd/lib/button/button";
 
 
 interface Props {
@@ -15,7 +17,6 @@ interface Props {
 
 
 interface State {
-  hiddenEditInput: boolean,
   input: string
 }
 
@@ -24,7 +25,6 @@ class TodoModal extends React.Component<Props, State> {
   constructor(props: Props, state: State) {
     super(props, state);
     this.state = {
-      hiddenEditInput: false,
       input: ""
     }
   }
@@ -33,6 +33,11 @@ class TodoModal extends React.Component<Props, State> {
     this.setState({
       input: ""
     });
+    if (nextProps.visible) {
+      setTimeout(() => {
+        (document.querySelector(".modal-input") as HTMLInputElement).focus();
+      }, 50)
+    }
   };
 
   public render(): React.ReactNode {
@@ -46,26 +51,31 @@ class TodoModal extends React.Component<Props, State> {
       >
         <p className={"gray-7 edit-subtitle"}>我在 {new Date(this.props.todo.addTime).toLocaleString()} 创建了</p>
         <p className={"bold gray-9"}>{this.props.todo.title}</p>
-        <ul>
-          {this.props.todo.list.map((item, index) => <li key={index}><Checkbox defaultChecked={item.finish}
-                                                                               onChange={e => item.finish = e.target.checked}/> {item.name}
+        <ul className={["todo-ul", this.props.todo.list.length && "border"].join(" ")}>
+          {this.props.todo.list.map((item, index) =>
+            <li key={index}>
+              <Checkbox defaultChecked={item.finish}
+                        onChange={e => item.finish = e.target.checked}/>
+              <span className={item.finish ? "under-line" : ""}>{item.name}</span>
+              <Button htmlType={"button"} size={"small"} type={"danger"} onClick={() => {
+                this.deleteItem(index)
+              }}>删除</Button>
           </li>)}
         </ul>
-        {!this.state.hiddenEditInput &&
-				<Input size={"small"}
-				       autoFocus={true}
-				       className={"modal-input"}
-				       value={this.state.input}
-				       onChange={e => {
-                 this.setState({input: e.target.value})
-               }}
-				       onKeyDown={e => this.addTODODetails(e, this.props.todo, this.state.input)}
-				/>}
-        <p className={"gray-7 edit-placeholder"} onClick={() => this.setState({hiddenEditInput: false})}>点击添加步骤</p>
-        <p className={"gray-6"}>设置分类</p>
-        <button onClick={() => this.sendEmmit(this.props.onDelete)}>删除</button>
-        <button onClick={() => this.sendEmmit(this.props.onSave)}>确定</button>
-        <button onClick={() => this.sendEmmit(this.props.onCancle)}>取消</button>
+        <Input size={"small"}
+               autoFocus={true}
+               className={"modal-input"}
+               value={this.state.input}
+               onChange={e => this.setState({input: e.target.value})}
+               onKeyDown={e => this.addTODODetails(e, this.props.todo, this.state.input)}
+               placeholder={"回车保存步骤"}
+        />
+        {/*<p className={"gray-6"}>设置分类</p>*/}
+        <div className="footer">
+          <Button htmlType={"button"} type={"danger"} onClick={() => this.sendEmmit(this.props.onDelete)}>删除</Button>
+          <Button htmlType={"button"} type={"primary"} onClick={() => this.sendEmmit(this.props.onSave)}>确定</Button>
+          <Button htmlType={"button"} onClick={() => this.sendEmmit(this.props.onCancle)}>取消</Button>
+        </div>
       </Modal>
     )
   }
@@ -82,6 +92,10 @@ class TodoModal extends React.Component<Props, State> {
     }
   }
 
+  private deleteItem(index: number) {
+    this.props.todo.list.splice(index, 1);
+    this.forceUpdate();
+  }
 }
 
 
