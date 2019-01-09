dfffa3ccef9b9d7d1554ff9de7bfc4782c1e4c05
AmberAAA <qe-d@163.com>
2019-01-09 13:24:24 +0800

    turn on server worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {unregister} from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-unregister();
+// unregister();
+registerServiceWorker();
